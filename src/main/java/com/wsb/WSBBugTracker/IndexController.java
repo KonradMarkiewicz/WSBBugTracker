a6dc83c624fb21de439c143dbdd4f8679172808b
konrad.markiewicz@logintegra.com <konrad.markiewicz@logintegra.com>
2021-04-09 19:57:05 +0200

    first commit, first template

---
+++ src/main/java/com/wsb/WSBBugTracker/IndexController.java
@@ -0,0 +1,22 @@
+package com.wsb.WSBBugTracker;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(
+            @RequestParam(name = "parameter")
+            String parameter,
+            Model model){
+        model.addAttribute("name","myszojeleń");
+        double number = 12+32+23.2;
+        model.addAttribute("number", number);
+        model.addAttribute("parameter", parameter);
+        return "index";
+    }
+}
