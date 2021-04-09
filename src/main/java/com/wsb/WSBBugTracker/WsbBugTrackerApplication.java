0801596d946d153e8b93376c1790d1f185a1a28f
konrad.markiewicz@logintegra.com <konrad.markiewicz@logintegra.com>
2021-04-09 19:24:37 +0200

    Initial commit

---
+++ src/main/java/com/wsb/WSBBugTracker/WsbBugTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.wsb.WSBBugTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsbBugTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsbBugTrackerApplication.class, args);
+	}
+
+}
