89b0fb80c8eb33badeec91cb77f25b29cb6f2c38
konrad.markiewicz@logintegra.com <konrad.markiewicz@logintegra.com>
2021-05-01 10:47:33 +0200

    WSBBugTracker-3 Obsługa dodawania nowego użytkownika

--- src/main/java/com/wsb/WSBBugTracker/auth/PersonController.java
+++ src/main/java/com/wsb/WSBBugTracker/auth/PersonController.java
@@ -3,10 +3,7 @@ package com.wsb.WSBBugTracker.auth;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
