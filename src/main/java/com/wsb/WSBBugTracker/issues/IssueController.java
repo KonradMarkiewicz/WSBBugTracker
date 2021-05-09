48807923b1d802fe4caf2c7e70f24704527544fc
konrad.markiewicz@logintegra.com <konrad.markiewicz@logintegra.com>
2021-05-09 14:20:26 +0200

    WSBBugTracker-37 Obsługa dodawania nowego zgłoszenia

--- src/main/java/com/wsb/WSBBugTracker/issues/IssueController.java
+++ src/main/java/com/wsb/WSBBugTracker/issues/IssueController.java
@@ -1,14 +1,21 @@
 package com.wsb.WSBBugTracker.issues;
 
+import com.wsb.WSBBugTracker.enums.Priority;
+import com.wsb.WSBBugTracker.enums.Type;
 import com.wsb.WSBBugTracker.people.PersonRepository;
 import com.wsb.WSBBugTracker.enums.State;
 import com.wsb.WSBBugTracker.projects.ProjectRepository;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/issues")
@@ -33,6 +40,42 @@ public class IssueController {
         modelAndView.addObject("people", personRepository.findAll());
         modelAndView.addObject("states", State.values());
         modelAndView.addObject("filter", issueFilter);
+
+        return modelAndView;
+    }
+
+    @GetMapping("/create")
+    @Secured("ROLE_CREATE_ISSUE")
+    ModelAndView create() {
+        ModelAndView modelAndView = new ModelAndView("issues/create");
+        return getModelAndView(modelAndView);
+    }
+
+    @PostMapping("/save")
+    @Secured("ROLE_CREATE_ISSUE")
+    ModelAndView createNewUser(@ModelAttribute @Valid Issue issue, BindingResult result,
+                               RedirectAttributes attributes) {
+        ModelAndView modelAndView = new ModelAndView();
+
+        if (result.hasErrors()) {
+            modelAndView.setViewName("issues/create");
+            return getModelAndView(modelAndView);
+        }
+
+        issueRepository.save(issue);
+//        attributes.addAttribute("create", "success");
+        modelAndView.setViewName("redirect:/issues");
+
+        return modelAndView;
+    }
+
+    private ModelAndView getModelAndView(ModelAndView modelAndView) {
+        modelAndView.addObject("projects", projectRepository.findAll());
+        modelAndView.addObject("people", personRepository.findAll());
+        modelAndView.addObject("states", State.values());
+        modelAndView.addObject("priorities", Priority.values());
+        modelAndView.addObject("types", Type.values());
+        modelAndView.addObject("issue", new Issue());
 
         return modelAndView;
     }
