931da009ae3deeda0a4d01e4d3a248dada19ebae
konrad.markiewicz@logintegra.com <konrad.markiewicz@logintegra.com>
2021-05-30 20:13:33 +0200

    WSBBugTracker-68 Refactor paginacji

--- src/main/java/com/wsb/WSBBugTracker/people/PersonRepository.java
+++ src/main/java/com/wsb/WSBBugTracker/people/PersonRepository.java
@@ -1,10 +1,13 @@
 package com.wsb.WSBBugTracker.people;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface PersonRepository extends JpaRepository<Person, Long> {
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long>, JpaSpecificationExecutor<Person> {
     Person findByUsername(String username);
     List<Person> findPersonByEnabledIsTrue();
 }
