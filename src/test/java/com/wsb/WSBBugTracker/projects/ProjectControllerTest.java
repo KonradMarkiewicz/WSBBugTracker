0386f05eb2e0c286bd2bc892dc19333cf175a907
konrad.markiewicz@logintegra.com <konrad.markiewicz@logintegra.com>
2021-06-04 19:11:27 +0200

    WSBBugTracker-43 Testy jednostkowe aplikacji

---
+++ src/test/java/com/wsb/WSBBugTracker/projects/ProjectControllerTest.java
@@ -0,0 +1,27 @@
+package com.wsb.WSBBugTracker.projects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ProjectControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @WithMockUser(username = "admin", password = "aA123456")
+    public void index() throws Exception {
+        mockMvc.perform(get("/projects"))
+                .andExpect(status().isOk());
+    }
+}
+
